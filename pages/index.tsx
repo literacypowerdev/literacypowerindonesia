263dad72a198f4f2911eac09a875ddcf616f7b85
ditramadia <ditrarizqaamadia@gmail.com>
2022-09-01 21:42:11 +0700

    Extended tailwind custom theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,32 @@
-import type { NextPage } from 'next'
-
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1 className="text-5xl font-bold underline">
-        Hello world!
-      </h1>
+      <p className="text-title1 font-bold text-main-green font-ptserif">
+        OUR PROJECTS
+      </p>
+      <p className="text-title2 font-bold text-main-orange font-ptserif">
+        Visi
+      </p>
+      <p className="text-title3 font-bold text-main-orange font-ptserif">
+        Cikarang
+      </p>
+      <p className="text-base font-medium text-main-orange font-ptserif">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos itaque ut,
+        voluptate reprehenderit ipsa necessitatibus assumenda aliquid suscipit
+        cupiditate rerum, accusamus doloremque quam ducimus veniam quasi fugit.
+        Porro quae rem unde in? Odit laborum eos sint harum magni autem
+        obcaecati recusandae, amet quaerat dolore enim. Aut atque harum suscipit
+        animi nulla praesentium quasi accusamus rem repudiandae? Sapiente
+        excepturi, expedita laboriosam ipsam, odio iusto aspernatur quo veniam
+        quae nihil ipsum ab repellat, provident blanditiis. Natus totam officiis
+        ratione, repellendus deleniti repellat doloremque rem sapiente.
+        Distinctio, deserunt voluptatibus. Facere eum maiores corporis unde
+        nihil temporibus itaque! Modi velit animi eligendi nobis vel?
+      </p>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        ptserif: "'PT Serif', serif;",
+      },
+      colors: {
+        "main-orange": "#DD8D5A",
+        "light-orange": "#E2AC86",
+        "very-light-orange": "#E4CFB4",
+        "main-green": "#708A81",
+        "dark-green": "#42524C",
+        "main-blue": "#1E3444",
+        "main-background": "#FBF9ED",
+      },
+      fontSize: {
+        title1: "3.2rem",
+        title2: "2.8rem",
+        title3: "2rem",
+        base: "1.4rem",
+      },
+    },
   },
   plugins: [],
-}
+};
