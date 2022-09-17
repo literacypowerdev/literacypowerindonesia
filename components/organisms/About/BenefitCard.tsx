0bf72e0cef9121c54d59ec35b9baff9c7a739009
ditramadia <ditrarizqaamadia@gmail.com>
2022-09-17 20:08:21 +0700

    Added documentation container

--- components/organisms/About/BenefitCard.tsx
+++ components/organisms/About/BenefitCard.tsx
@@ -12,7 +12,9 @@ export default function BenefitCard(props: BenefitCardProps) {
   return (
     <div className="w-full max-w-[520px] min-h-[150px] sm:min-h-[90px] bg-white shadow-md rounded-[18px] flex items-center justify-center sm:justify-start flex-col sm:flex-row py-4 px-2 sm:px-6 gap-3 sm:gap-6">
       <img src={imageUrl} width={50} height={50} alt="Benefit icon" />
-      <p className="text-center sm:text-left">{text}</p>
+      <p className="text-center sm:text-left font-ptserif text-main-blue">
+        {text}
+      </p>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,14 @@ import Footer from "../components/organisms/Footer";
 import Hero from "../components/organisms/Hero";
 import Navbar from "../components/organisms/Navbar";
 import Newsletter from "../components/organisms/Newsletter";
+import Documentation from "../components/organisms/Documentation";
 
 const Home: NextPage = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Documentation />
       <About />
       <Newsletter />
       <Footer />
