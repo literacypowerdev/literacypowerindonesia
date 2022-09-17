6b917617a9a6266a2cc1d1d1dbca585597af3035
ditramadia <ditrarizqaamadia@gmail.com>
2022-09-17 21:13:16 +0700

    Added documentation swiper

--- components/organisms/Documentation/index.tsx
+++ components/organisms/Documentation/index.tsx
@@ -1,17 +1,22 @@
 import React from "react";
+import DocSlider from "./Slider";
 
 export default function Documentation() {
   return (
     <div className="w-[280px] md:w-[540px] xl:w-[800px] mx-auto -mt-[120px] rounded-[18px] overflow-hidden shadow-xl">
       <div className="w-full px-3 sm:px-5 py-1 sm:py-2 bg-main-green flex justify-between items-center">
         <h2 className="text-white font-ptserif font-bold">Documentation</h2>
-        <div className="flex gap-1">
+        <div className="flex gap-1 md:gap-2">
           <div className="w-[18px] h-[18px] bg-very-light-orange rounded-full"></div>
           <div className="w-[18px] h-[18px] bg-main-orange rounded-full"></div>
           <div className="w-[18px] h-[18px] bg-dark-green rounded-full"></div>
         </div>
       </div>
-      <div className="w-full h-[200px] xl:h-[300px] bg-white"></div>
+      <div className="w-full py-5 md:px-10 h-[280px] md:h-[200px] xl:h-[290px] bg-white">
+        <div className="w-full">
+          <DocSlider />
+        </div>
+      </div>
     </div>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import "../styles/globals.css";
+import "/styles/slick/slick.css";
+import "/styles/slick/slick-theme.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
