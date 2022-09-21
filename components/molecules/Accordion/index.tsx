159221fd4fc3c0e164b71e89d820eb0eb2a28c2f
Ditra Amadia <60308058+ditramadia@users.noreply.github.com>
2022-09-21 17:25:12 +0700

    Added apply page (#14)

---
+++ components/molecules/Accordion/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+export interface AccordionProps {
+  title: string;
+  steps: string[];
+}
+
+export default function Accordion(prop: AccordionProps) {
+  const { title, steps } = prop;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="w-full flex flex-col gap-1 ">
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className="w-full bg-main-orange mb-1 hover:bg-light-orange transition-all duration-150 ease-in-out cursor-pointer px-5 py-2 rounded-[20px] flex justify-between shadow-md"
+      >
+        <h2 className="text-white font-ptserif font-bold">{title}</h2>
+        <img src="/icon/accordion-arrow.svg" alt="" className="w-[16px]" />
+      </div>
+      <div
+        className={`w-full flex flex-col gap-1 mb-5 ${isOpen ? "" : "hidden"}`}
+      >
+        {steps.map((item, index) => {
+          return (
+            <div
+              key=""
+              className="w-full bg-white px-5 py-2 rounded-[20px] shadow-md flex justify-center"
+            >
+              <h2 className="text-main-orange font-ptserif text-center">
+                {index + 1}. {item}
+              </h2>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
