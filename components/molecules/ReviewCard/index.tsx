aedef5fd0d0219224a8f36ef336e7bc407d7e113
ditramadia <ditrarizqaamadia@gmail.com>
2022-09-21 11:42:27 +0700

    Updated text color

--- components/molecules/ReviewCard/index.tsx
+++ components/molecules/ReviewCard/index.tsx
@@ -13,13 +13,13 @@ export default function ReviewCard(props: ReviewCardProps) {
   return (
     <div className="w-full max-w-[1100px] text-center mx-auto h-fit bg-white shadow-lg my-5 rounded-[20px] overflow-hidden p-4 pb-6 flex flex-col gap-3 md:gap-2 transition-all duration-150 ease-in-out">
       <div>
-        <h3 className="font-ptserif font-bold">{name}</h3>
-        <p className="font-ptserif">{occupation}</p>
+        <h3 className="font-ptserif font-bold text-main-blue">{name}</h3>
+        <p className="font-ptserif text-main-blue">{occupation}</p>
       </div>
       <h2 className="font-ptserif font-bold text-base text-main-orange">
         {title}
       </h2>
-      <p>“{content}”</p>
+      <p className="font-ptserif text-main-blue">“{content}”</p>
     </div>
   );
 }
