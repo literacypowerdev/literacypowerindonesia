4a5e5d77a6dbb768932304aeadb81d5436a5db31
mhikmal03 <hikmalkp@gmail.com>
2023-02-19 22:20:11 +0700

    .

--- components/organisms/Project/index.tsx
+++ components/organisms/Project/index.tsx
@@ -89,12 +89,14 @@ export default function Projects() {
       },
     ],
   ];
-
+  const [loading, setLoading] = useState<boolean>(true)
   const [data, setData] = useState([]);
   const [pageNumber, setPageNumber] = useState(0);
   const fetchData = async () => {
     const response = await axios.get(`http://localhost:4500/api/article/pagination?page=${pageNumber}&table=proyek&pageSize=3`);
     setData(response.data);
+    setLoading(false)
+    
   };
   useEffect(() => {
     fetchData();
@@ -102,70 +104,69 @@ export default function Projects() {
 
   return (
     <>
-      {data.length > 0 ? (
-        data.map((item: any) => {
-          const dateStr = item.tanggal;
-          const date = new Date(dateStr);
-          const dayOfMonth = date.getDate();
-          const month = new Intl.DateTimeFormat('en-US', { month: 'short' }).format(date);
-          const year = date.getFullYear();
-          const formattedDate = `${dayOfMonth} ${month} ${year}`;
-          return (
-
-
-            <ProjectCard
-              key={item.id}
-              id={item.id}
-              thumbnail={item.dokumentasi}
-              name={item.nama}
-              location={item.lokasi}
-              date={formattedDate}
-              content={item.content}
-            />
-          );
-        })
-      ) : (
+      {loading ? (
         <div className='text-center'>
-          <h1>There is no data</h1>
+          <h1>Loading...</h1>
         </div>
+      ) : (
+        data.length > 0 && !loading ? (
+          data.map((item: any) => {
+            const dateStr = item.tanggal;
+            const date = new Date(dateStr);
+            const dayOfMonth = date.getDate();
+            const month = new Intl.DateTimeFormat('en-US', { month: 'short' }).format(date);
+            const year = date.getFullYear();
+            const formattedDate = `${dayOfMonth} ${month} ${year}`;
+            return (
+              <ProjectCard
+                key={item.id}
+                id={item.id}
+                thumbnail={item.dokumentasi}
+                name={item.nama}
+                location={item.lokasi}
+                date={formattedDate}
+                content={item.content}
+              />
+            );
+          })
+        ) : ''
       )}
-      
-        <ReactPaginate
+      <ReactPaginate
         onClick={handleScrollToTop}
-          previousLabel={"<"}
-          nextLabel={">"}
-          breakLabel={"..."}
-          pageCount={3}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={3}
-          onPageChange={(data) => {
-            setPageNumber(data.selected);
-          }}
-          containerClassName={"flex gap-2 mt-2 lg:mt-12 justify-center"}
-          activeClassName={"underline bg-main-orange "}
-          pageClassName={
-            "w-7 h-7 bg-very-light-orange rounded-lg hover:bg-main-orange"
-          }
-          pageLinkClassName={
-            "text-white font-ptserif w-full h-full flex justify-center items-center"
-          }
-          previousClassName={
-            "w-7 h-7 bg-very-light-orange rounded-lg hover:bg-main-orange"
-          }
-          previousLinkClassName={
-            "text-white font-ptserif w-full h-full flex justify-center items-center"
-          }
-          nextClassName={
-            "w-7 h-7 bg-very-light-orange rounded-lg hover:bg-main-orange"
-          }
-          nextLinkClassName={
-            "text-white font-ptserif w-full h-full flex justify-center items-center"
-          }
-          breakClassName={"w-7 h-7"}
-          breakLinkClassName={
-            "text-very-light-orange font-ptserif w-full h-full flex justify-center items-center cursor-default"
-          }
-        />
+        previousLabel={"<"}
+        nextLabel={">"}
+        breakLabel={"..."}
+        pageCount={3}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={(data) => {
+          setPageNumber(data.selected);
+        }}
+        containerClassName={"flex gap-2 mt-2 lg:mt-12 justify-center"}
+        activeClassName={"underline bg-main-orange "}
+        pageClassName={
+          "w-7 h-7 bg-very-light-orange rounded-lg hover:bg-main-orange"
+        }
+        pageLinkClassName={
+          "text-white font-ptserif w-full h-full flex justify-center items-center"
+        }
+        previousClassName={
+          "w-7 h-7 bg-very-light-orange rounded-lg hover:bg-main-orange"
+        }
+        previousLinkClassName={
+          "text-white font-ptserif w-full h-full flex justify-center items-center"
+        }
+        nextClassName={
+          "w-7 h-7 bg-very-light-orange rounded-lg hover:bg-main-orange"
+        }
+        nextLinkClassName={
+          "text-white font-ptserif w-full h-full flex justify-center items-center"
+        }
+        breakClassName={"w-7 h-7"}
+        breakLinkClassName={
+          "text-very-light-orange font-ptserif w-full h-full flex justify-center items-center cursor-default"
+        }
+      />
     </>
   );
 }
