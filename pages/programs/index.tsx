214cb50539344662d3a34a59f22efbfeb7fc92e1
Ditra Amadia <ditrarizqaamadia@gmail.com>
2023-09-24 21:03:33 +0700

    add program into navbar

--- pages/programs/index.tsx
+++ pages/programs/index.tsx
@@ -17,7 +17,7 @@ export default function Programs() {
         />
         <link rel="icon" sizes="192x192" href="/icon/favicon.ico"></link>
       </Head>
-      <Navbar active="Projects" />
+      <Navbar active="Programs" />
       <div className="w-full bg-body-doodle bg-contain bg-repeat py-7 lg:pb-20">
         <div className="w-11/12 md:w-10/12 mx-auto">
           <h1 className="text-title3 my-2 lg:my-12 font-bold font-ptserif text-center text-main-green">
